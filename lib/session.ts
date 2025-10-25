9fcd62ed3224552cca1bdc5d4786203883763505
Felix Ripper <77970275+felixripper@users.noreply.github.com>
2025-10-25 11:36:12 +0300

    Enhance session management with custom types
    Refactor session handling to include custom session data interface and update session options.

--- lib/session.ts
+++ lib/session.ts
@@ -1,16 +1,20 @@
-import type { IronSessionOptions } from 'iron-session'
+import { getIronSession, IronSession, IronSessionData } from 'iron-session';
+import { cookies } from 'next/headers';
 
-export const sessionOptions: IronSessionOptions = {
+export const sessionOptions = {
   password: process.env.SECRET_COOKIE_PASSWORD as string,
   cookieName: 'snakebase-admin-session',
   cookieOptions: {
     secure: process.env.NODE_ENV === 'production',
   },
+};
+
+// Bu, session objemizin tipini belirtir
+export interface SessionData extends IronSessionData {
+  isLoggedIn?: boolean;
 }
 
-// Bu, session objesinin tipini belirtir
-declare module 'iron-session' {
-  interface IronSessionData {
-    isLoggedIn?: boolean
-  }
+// Bu, oturumu almak için kullanacağımız merkezi fonksiyondur
+export function getSession(): Promise<IronSession<SessionData>> {
+  return getIronSession<SessionData>(cookies(), sessionOptions);
 }
